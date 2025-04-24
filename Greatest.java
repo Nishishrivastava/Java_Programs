05e99239a7b34e514ba7a02308bf30437f477313
Nishishrivastava02 <83336594+Nishishrivastava@users.noreply.github.com>
2025-04-24 17:14:57 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,15 @@
+public class Greatest {
+    public static void main(String[] args) {
+
+        int n1 = 20, n2 = 21, n3 = 24;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
